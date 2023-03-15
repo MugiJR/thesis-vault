1efec237d54cd27f129c29910f1b3e794fbbf110
MugiJR <immugi@yahoo.com>
2023-03-15 18:40:37 +0100

    Included javaparsers expr,stmt,type and body

--- app/src/main/java/thesis/vault/App.java
+++ app/src/main/java/thesis/vault/App.java
@@ -9,20 +9,15 @@ import com.github.javaparser.StaticJavaParser;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Optional;
 import java.util.List;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.expr.Name;
-import com.github.javaparser.ast.expr.SimpleName;
+
+import com.github.javaparser.ast.body.EnumConstantDeclaration;
+import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.stmt.*;
+import com.github.javaparser.ast.type.UnknownType;
+import com.github.javaparser.ast.type.VarType;
 import com.github.javaparser.ast.type.VoidType;
 import com.github.javaparser.ast.Modifier;
-import com.github.javaparser.ast.expr.LiteralExpr;
-import com.github.javaparser.ast.stmt.ContinueStmt;
-import com.github.javaparser.ast.stmt.UnparsableStmt;
-import com.github.javaparser.ast.stmt.EmptyStmt;
 import com.github.javaparser.ast.comments.LineComment;
 import com.github.javaparser.ast.comments.BlockComment;
 import com.github.javaparser.ast.type.PrimitiveType;
@@ -37,22 +32,42 @@ public class App {
             }
             if (node instanceof Name ||
                 node instanceof SimpleName ||
-                node instanceof Modifier ||
                 node instanceof VoidType ||
                 node instanceof Modifier ||
                 node instanceof LiteralExpr ||
                 node instanceof ContinueStmt ||
                 node instanceof LineComment ||
                 node instanceof BlockComment ||
-                node instanceof PrimitiveType) {
+                node instanceof PrimitiveType ||
+                node instanceof AnnotationExpr ||
+                node instanceof ClassExpr ||
+                node instanceof EnclosedExpr ||
+                node instanceof SuperExpr ||
+                node instanceof ThisExpr ||
+                node instanceof TypeExpr ||
+                node instanceof UnknownType ||
+                node instanceof VarType ||
+                node instanceof BreakStmt ||
+                node instanceof EmptyStmt ||
+                node instanceof ReturnStmt ||
+                node instanceof EnumConstantDeclaration
+            ) {
                 sb.append("{\"type\":\"")
                     .append(node.getClass().getName())
                     .append("\", \"data\":\"")
                     .append(node.toString().replace("\\", "\\\\")
-                                .replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\""));
+                                .replace("\n", "\\n")
+                                .replace("\r", "\\r")
+                                .replace("\"", "\\\""));
+
                     if (node instanceof Name ||
                         node instanceof ContinueStmt ||
-                        node instanceof PrimitiveType) {
+                        node instanceof PrimitiveType ||
+                        node instanceof LiteralExpr ||
+                        node instanceof ReturnStmt ||
+                        node instanceof EnumConstantDeclaration
+                    ) {
+
                         sb.append("\", \"child\":");
                         toJson(node.getChildNodes(), sb);
                         sb.append("}");
@@ -73,8 +88,10 @@ public class App {
         }
         sb.append("]");
     }
+
+
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        System.out.println("ARGS Path : " + args[0]);
         try {
             StringBuilder sb = new StringBuilder();
             //"*.java"
