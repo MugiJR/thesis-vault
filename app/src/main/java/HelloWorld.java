c9b2a49a0c62138164eb9c2ab4eb01f25c28d664
MugiJR <immugi@yahoo.com>
2023-05-21 12:19:37 +0200

    Added example file for javaparser library and test file with updated gradle file

---
+++ app/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * This is a simple Java program that prints "Hello, world!" to the console.
+ */
+public class HelloWorld {
+
+    /**
+     * This is the main method, which is the entry point of the program.
+     * It prints "Hello, world!" to the console.
+     * @param args The command-line arguments, which are not used in this program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!"); // Print "Hello, world!" to the console
+    }
+}
